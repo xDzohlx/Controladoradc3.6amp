b931279f49574fac0bb02b736180113b8a38d219
Carlos Vega <69025860+xDzohlx@users.noreply.github.com>
2021-02-22 23:50:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 /*
- * Controladora con ppm.cpp
+ * controladora828.cpp
  *
- * Created: 28/07/2020 19:50:39
- * Author : Usuario
+ * Created: 23/05/2017 23:52:50
+ * Author : dzohl
  */ 
 
-//input canal 1 pa0,
-//ouput decoder canal 2 pa1 
+//detalles de version
+//todavia no esta bien saturados los canales
+//
+//version acceptable y estable faltan limites inferiores se arregla desde el control con endpoints
+
+
+//inputs canal 1 pa2, canal 2 pa3
 //motores 
 
-//funciona la lectura de canales pero no la traccion parece que solo lee el canal 2
-#define F_CPU 8000000
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -33,39 +36,46 @@ volatile static uint16_t giro_control_2 = 0x00;
 volatile static uint16_t tiempo1 = 0x0000;
 volatile static uint16_t tiempo2 = 0x0000;
 volatile static uint16_t tiempo3 = 0x0000;
-volatile static uint16_t canal[6];
-//volatile static uint16_t canal_offset[5];
 volatile static uint16_t canal_3 = 0x0000;
 volatile static uint8_t motor1pwm = 0x00;
 volatile static uint8_t motor2pwm = 0x00;
 volatile static uint8_t ledpwm[2];
 uint16_t offsetgiro = 0x00;
 uint16_t offsetaccel = 0x00;
-int cont = 0;
 
-bool button = true; 
 bool primero = false;
 bool segundo = false;
 bool tercero = false;
+bool button = true; 
+bool canal = true; 
+bool canal_1 = false;
+bool canal_2 = false;
+bool select_canal_1 = false;
+bool select_canal_2 = false;
 bool seleccion = false;
 bool forward = false;
 bool adelante = false;
 bool forward_canal_2 = false;
 bool right = false;
 bool superbutton = false;
-//bool failsafe = true;
+bool canal_1_valido = false;
+bool canal_2_valido = false;
+bool canal_3_valido = false;
+bool failsafe = true;
 
 
 void setup(){	
-
 	//dos canales controlados por interrupcion
-	DDRA = (1<<PORTA1);//
-	DDRC = (1<<PORTC4)|(1<<PORTC0)|(1<<PORTC3)|(1<<PORTC1)|(1<<PORTC6);
+	DDRC = (1<<DDC4)|(1<<PORTC0)|(1<<PORTC3)|(1<<PORTC1)|(1<<PORTC6);
 	PUEB = (1<<PORTB6)|(1<<PORTB4);
 	PORTB = (1<<PORTB6)|(1<<PORTB4);
 	PORTC |= (1<<PORTC6);
+	//DDRA = (1<<PORTA0)|(1<<PORTA1);
+	//PORTA |= (1<<PORTA1);//robot encendido
+	//selecciondecanales();
+	//interrupciones pines externos canal1
 	PCICR = (1<<PCIE0);
-	PCMSK0 |= (1<<PCINT0);//|(1<<PCINT1);//|(1<<PCINT4);//interrucpciones canales
+	PCMSK0 |= (1<<PCINT0)|(1<<PCINT1);//|(1<<PCINT4);//interrucpciones canales
 	//timer0 8 bit
 	TCCR0A |= (1<<COM0A0)|(1<<COM0A1)|(1<<WGM00)|(1<<WGM01)|(1<<COM0B1)|(1<<COM0B0);
 	TCCR0B |= (1<<CS00);
@@ -95,74 +105,138 @@ void setup(){
 	TCCR1A = 0x00;
 	TCCR1B = (1<<WGM12)|(1<<CS11);//|(1<<CS10);
 	
-	OCR1A = 0xFA0;//validador de rc checar failsafe 898
+	OCR1A = 0xFF00;//validador de rc checar failsafe
 	
-	TIMSK1 = (1<<OCIE1A);//(1<<TOIE1)|
+	//TIMSK1 = (1<<TOIE1)|(1<<OCIE1A);
 	
 	TCNT1 = 0x0000;
 	//selecciondecanales();
 	sei();
 	//parchada 
-
-	_delay_ms(5000);
-	giro_val = canal[0];
-	_delay_ms(100);
-	accel_val = canal[1];
+	_delay_ms(2000);
+	_delay_ms(2000);
+	_delay_ms(2000);
+	_delay_ms(2000);
+	_delay_ms(2000);
+	giro_val = giro;
+	filtro_giro = giro_val;
+	_delay_ms(2000);
+	accel_val = accel;
+	filtro_accel = accel_val;
+}
+void pwmled(){
+	if (TCNT0>=0&&primero){
+		//PORTC |= (1<<PORTC6);
+		primero = false;
+		ledpwm[0]++;
+		if (segundo){
+			PORTC |= (1<<PORTC6);
+			}else{
+			PORTC &= ~(1<<PORTC6);
+		}
+	}
+	if (TCNT0>=ledpwm[0]&&!primero){
+		//PORTC &= ~(1<<PORTC6);
+		primero = true;
+		if (segundo){
+			PORTC &= ~(1<<PORTC6);
+			}else{
+			PORTC |= (1<<PORTC6);
+		}
+	}
+	if (ledpwm[0]==255&&tercero){
+		segundo = !segundo;
+		tercero=false;
+	}
+	if (ledpwm[0]==0){
+		tercero = true;
+	}
 }
 //INTERRUPCIONES PARA LECTURA DE RC
-void pwmled(){
-			if (TCNT0==255&&primero){
-				ledpwm[0]++;
-				primero = false;
-			}
-			if(TCNT0!=255){
-				primero=true;
-			}
-			if (ledpwm[0]==255&&segundo){
-				ledpwm[1]++;
-				segundo = false;
-			}
-			if(ledpwm[0]!=255){
-				segundo = true;
-			}
-			if (ledpwm[1]==255){
-				tercero = !tercero;	
-			}
-			if (TCNT0==0){
-				if (tercero){
-					PORTC |= (1<<PORTC6);
-				}else{
-					PORTC &= ~(1<<PORTC6);
-				}
-			}
-			if (ledpwm[1]>=TCNT0){
-				if (tercero){
-					PORTC &= ~(1<<PORTC6);
-					}else{
-					PORTC |= (1<<PORTC6);
-				}
-			}
-}
+//inputs canal 1 pa2, canal 2 pa3
 ISR(PCINT0_vect){
-if (!(PINA & 0x01)){
-	if (cont > 0){
-		canal[cont -1]=TCNT1;
-		giro = canal[0];
-		accel = canal[1];
+//funciona de forma sincrona ESTA PARCHADO
+if (!(PINA & 0x02)){
+	if (canal_1_valido){
+		if(tiempo1< TCNT1)//ESTO ES UN PARCHE
+		giro = TCNT1 - tiempo1;//lectura 1
+		canal_1_valido = false;
+		failsafe  = false;
 	}
-	TCNT1 = 0x00;
-	cont++;
+} else{
+	if (canal){
+		TCNT1 = 0x0000;
+		canal = false;
+	}
+	if (!canal_1_valido){
+		tiempo1 = TCNT1;//lectura 2
+		canal_1_valido = true;
+	}
 }
+//lectura 2
+if (!(PINA & 0x01)){
+	if (canal_2_valido){
+		if(tiempo2< TCNT1)//ESTO ES UN PARCHE
+		accel = TCNT1 - tiempo2;//lectura 1
+		canal_2_valido = false;
+	}
+	} else{
+	if (canal){
+		TCNT1 = 0x0000;
+		canal = false;
+	}
+	if (!canal_2_valido){
+		tiempo2 = TCNT1;//lectura 2
+		canal_2_valido = true;
+	}
+	//reset contador
+	if (!(PINA & 0x02)&&!(PINA & 0x01)){
+		canal = true;
+		TCNT1 = 0x0000;
+	}
+}
+//arm
+//if (!(PINA & 0x10)){
+	//if (canal_3_valido){
+		//canal_3 = TCNT1 - tiempo3;
+		//canal_3_valido = false;
+	//}
+	//} else{
+	//if (!canal_3_valido){
+		//tiempo3 = TCNT1;//lectura 2
+		//canal_3_valido = true;
+	//}
+//}
 }
 //contador useg
 ISR(TIMER1_COMPA_vect){
-	cont = 0;
+	failsafe  = true;
 }
 int main(void){
 	setup();
+	//PORTA &= ~(1<<PORTA1);
 	while(1){
+		//falta failsafe
 	while(1){//while(canal_3>1300){//probar diferencial
-
+		//while (failsafe)
+		//{
+			//OCR0A = 0x00;//hard stop
+			//OCR0B = 0x00;
+			//TOCPMCOE &= ~(1<<TOCC0OE);//motor a
+			//TOCPMCOE &= ~(1<<TOCC1OE);
+			//TOCPMCOE &= ~(1<<TOCC3OE);//motor a
+			//TOCPMCOE &= ~(1<<TOCC4OE);
+		//}
+			
+		//PORTA |= (1<<PORTA0);//arm amarillo
+		
+		//falta parchar esta parte
+		//if (giro>=giro_val){
+		//giro_control = giro - giro_val;//offset
+		//}else{
+		//giro_control = giro_val - giro;//offset
+		//}
+		//esta bugeado en la saturacion
 		if (giro>= giro_val){
 		giro_control = giro - giro_val;//offset
 			if (giro_control>accel){
@@ -178,13 +252,20 @@ int main(void){
 				}else{
 				accel_control_2 = accel - giro_control;//motor izquierdo
 			}
-			accel_control = accel + giro_control;//motor izquierdo			
+			accel_control = accel + giro_control;//motor izquierdo
+								
 		}
 
+//		giro_control = giro - giro_val;//offset
+		
+//		giro_control_2 = giro_val - giro;//offset
+		
+//		accel_control = accel + giro_control;//motor izquierdo
+		
+//		accel_control_2 = accel + giro_control_2;//motor derecho
 		//_------------------------
 		if (accel_control>=accel_val){//MOTOR A
 			//PORTA &= ~(1<<PORTA1);//arm
-				//PORTC |= (1<<PORTC6);
 			accel_val_2 = accel_control - accel_val;
 			if (accel_val_2>510)
 			{
@@ -197,7 +278,6 @@ int main(void){
 			//PORTC |= (1<<PORTC1);//arm
 			OCR0A = accel_val_2;
 			}else{
-					//PORTC &= ~(1<<PORTC6);
 				accel_val_2=accel_val-accel_control;
 				if (accel_val_2>510)
 				{
@@ -237,8 +317,8 @@ int main(void){
 			TOCPMCOE &= ~(1<<TOCC3OE);
 			//TCCR0A &= ~(1<<COM0A0);
 			OCR0B = giro_val_2;
-		}
-pwmled();
+		}	
+		pwmled();
 		}//arm
 	OCR0A = 0x00;//hard stop
 	OCR0B = 0x00;
